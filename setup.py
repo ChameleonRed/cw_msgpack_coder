759ca7457733686900e40bf56fbbc52284f8e3fc
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2018-01-03 21:01:05 +0100

    added hello world example

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def long_description():
 
 
 setup(name='cw_msgpack_coder',
-      version='1.1.1',
+      version='1.1.2',
       description='Simple and fast Python any object serialization with use msgpack.',
       long_description=long_description(),
       author='Cezary K. Wagner',
